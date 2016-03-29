811dd6bd58ad32226482387f5cb518cbada41577
venkatp1997 <venkat.p1997@gmail.com>
2016-03-29 23:49:34 +0530

    Final Game

--- js/main.js
+++ js/main.js
@@ -64,6 +64,7 @@ for(var i=0;i<BLACK_COINS_NO;i++){
 	BLACK_Z[i]=Math.cos(BLACK_ANGLE[i])*INNER_CIRCLE1_RADIUS;
 	black_sphere.position.set(BLACK_X[i],BLACK_Y[i],BLACK_Z[i]);
 	scene.add(black_sphere);
+	BLACK_SPHERE.push(black_sphere);
 }
 for(var i=0;i<WHITE_COINS_NO;i++){
 	var white_sphere=new THREE.Mesh(coin_geometry,white_sphere_material);
@@ -72,6 +73,7 @@ for(var i=0;i<WHITE_COINS_NO;i++){
 	WHITE_Z[i]=Math.cos(WHITE_ANGLE[i])*INNER_CIRCLE1_RADIUS;
 	white_sphere.position.set(WHITE_X[i],WHITE_Y[i],WHITE_Z[i]);
 	scene.add(white_sphere);
+	WHITE_SPHERE.push(white_sphere);
 }
 
 //Striker
@@ -117,8 +119,6 @@ function updateStriker(){
 			break;
 	}
 	if(STRIKER_MOV==1){
-		var STRIKER_VELOCITYX=STRIKER_VELOCITY*Math.cos(STRIKER_ANGLE);
-		var STRIKER_VELOCITYZ=STRIKER_VELOCITY*Math.sin(STRIKER_ANGLE);
 		if(STRIKER_X>STRIKER_MAXX || STRIKER_X<STRIKER_MINX){
 			STRIKER_VELOCITYX_F*=-1;
 		}
@@ -132,6 +132,7 @@ function updateStriker(){
 		STRIKER_VELOCITY=Math.max(STRIKER_VELOCITY-FRICTION,0);
 		if(STRIKER_VELOCITY==0){
 			striker_init();
+			add_newarrow();
 		}
 	}
 	striker.position.set(STRIKER_X,STRIKER_Y,STRIKER_Z);
@@ -169,7 +170,173 @@ function updateCursor(){
 	cursor.position.set(CURSOR_X, CURSOR_Y, CURSOR_Z);  
 	scene.add(cursor);
 }
+function coin_coin(flag_1,i,flag_2,j){
+	if(flag_1==1){
+		x1=BLACK_X[i];
+		xv1=BLACK_VELOCITYX[i];
+		z1=BLACK_Z[i];
+		zv1=BLACK_VELOCITYZ[i];
+	}
+	else{
+		x1=WHITE_X[i];
+		xv1=WHITE_VELOCITYX[i];
+		z1=WHITE_Z[i];
+		zv1=WHITE_VELOCITYZ[i];
+	}
+	if(flag_2==1){
+		x2=BLACK_X[j];
+		xv2=BLACK_VELOCITYX[j];
+		z2=BLACK_Z[j];
+		zv2=BLACK_VELOCITYZ[j];
+	}
+	else{
+		x2=WHITE_X[j];
+		xv2=WHITE_VELOCITYX[j];
+		z2=WHITE_Z[j];
+		zv2=WHITE_VELOCITYZ[j];
+	}
+	if(Math.sqrt(Math.pow((x1-x2),2)+Math.pow((z1-z2),2))<(COIN_RADIUS+COIN_RADIUS)){
+		var diffZ=Math.abs(z1-z2);
+		var diffX=Math.abs(x1-x2);
+		var alpha=Math.atan(diffZ/diffX);
+		var velSA = xv1*Math.cos(alpha) + zv1*Math.sin(alpha);	
+		var velSP = zv1*Math.cos(alpha) - xv1*Math.sin(alpha);
+		var velCA = xv2*Math.cos(alpha) + zv2*Math.sin(alpha); 
+		var velCP = zv2*Math.cos(alpha) - xv2*Math.sin(alpha);
 
+		var temp = (1- COEFFICIENT_RES)*velCA + (1+COEFFICIENT_RES)*velSA;
+		velSA = ((1-COEFFICIENT_RES)*velSA + (1+COEFFICIENT_RES)*velCA)/2 , velCA = temp/2;
+
+		xv1=velSA*Math.cos(alpha)-velSP*Math.sin(alpha),zv1=velSP*Math.cos(alpha)+velSA*Math.sin(alpha);
+		xv2 = velCA*Math.cos(alpha) - velCP*Math.sin(alpha);
+		zv2 = velCP*Math.cos(alpha) + velCA*Math.sin(alpha);
+	}	
+	if(flag_1==1){
+		BLACK_VELOCITYX[i]=xv1;
+		BLACK_VELOCITYZ[i]=zv1;
+	}
+	else{
+		WHITE_VELOCITYX[i]=xv1;
+		WHITE_VELOCITYZ[i]=zv1;
+	}
+	if(flag_2==1){
+		BLACK_VELOCITYX[j]=xv2;
+		BLACK_VELOCITYZ[j]=zv2;
+	}
+	else{
+		WHITE_VELOCITYX[j]=xv2;
+		WHITE_VELOCITYZ[j]=zv2;
+	}
+}
+function updateCollisions(){
+	for(var i=0;i<BLACK_COINS_NO;i++){
+		if(Math.sqrt(Math.pow((STRIKER_X-BLACK_X[i]),2)+Math.pow((STRIKER_Z-BLACK_Z[i]),2))<(STRIKER_RADIUS+COIN_RADIUS)){
+			var diffZ=Math.abs(STRIKER_Z-BLACK_Z[i]);
+			var diffX=Math.abs(STRIKER_X-BLACK_X[i]);
+			var alpha=Math.atan(diffZ/diffX);
+			var velSA = STRIKER_VELOCITYX*Math.cos(alpha) + STRIKER_VELOCITYZ*Math.sin(alpha);	
+			var velSP = STRIKER_VELOCITYZ*Math.cos(alpha) - STRIKER_VELOCITYX*Math.sin(alpha);
+			var velCA = BLACK_VELOCITYX[i]*Math.cos(alpha) + BLACK_VELOCITYZ[i]*Math.sin(alpha); 
+			var velCP = BLACK_VELOCITYZ[i]*Math.cos(alpha) - BLACK_VELOCITYX[i]*Math.sin(alpha);
+
+			var temp = (1- COEFFICIENT_RES)*velCA + (1+COEFFICIENT_RES)*velSA; // switch S with C and visa versa
+			velSA = ((1-COEFFICIENT_RES)*velSA + (1+COEFFICIENT_RES)*velCA)/2 , velCA = temp/2; // and these
+
+			STRIKER_VELOCITYX=velSA*Math.cos(alpha)-velSP*Math.sin(alpha),STRIKER_VELOCITYZ=velSP*Math.cos(alpha)+velSA*Math.sin(alpha);
+			BLACK_VELOCITYX[i] = velCA*Math.cos(alpha) - velCP*Math.sin(alpha);
+			BLACK_VELOCITYZ[i] = velCP*Math.cos(alpha) + velCA*Math.sin(alpha);
+		}	
+		for(var j=0;j<BLACK_COINS_NO;j++){
+			if(i!=j){
+				if(Math.sqrt(Math.pow((BLACK_X[j]-BLACK_X[i]),2)+Math.pow((BLACK_Z[j]-BLACK_Z[i]),2))<(COIN_RADIUS+COIN_RADIUS)){
+					coin_coin(1,i,1,j);
+				}
+			}
+		}
+		for(var j=0;j<WHITE_COINS_NO;j++){
+			coin_coin(1,i,0,j);
+		}
+	}
+	for(var i=0;i<WHITE_COINS_NO;i++){
+		if(Math.sqrt(Math.pow((STRIKER_X-WHITE_X[i]),2)+Math.pow((STRIKER_Z-WHITE_Z[i]),2))<(STRIKER_RADIUS+COIN_RADIUS)){
+			var diffZ=Math.abs(STRIKER_Z-WHITE_Z[i]);
+			var diffX=Math.abs(STRIKER_X-WHITE_X[i]);
+			var alpha=Math.atan(diffZ/diffX);
+			var velSA = STRIKER_VELOCITYX*Math.cos(alpha) + STRIKER_VELOCITYZ*Math.sin(alpha);	
+			var velSP = STRIKER_VELOCITYZ*Math.cos(alpha) - STRIKER_VELOCITYX*Math.sin(alpha);
+			var velCA = WHITE_VELOCITYX[i]*Math.cos(alpha) + WHITE_VELOCITYZ[i]*Math.sin(alpha); 
+			var velCP = WHITE_VELOCITYZ[i]*Math.cos(alpha) - WHITE_VELOCITYX[i]*Math.sin(alpha);
+
+			var temp = (1- COEFFICIENT_RES)*velCA + (1+COEFFICIENT_RES)*velSA; // switch S with C and visa versa
+			velSA = ((1-COEFFICIENT_RES)*velSA + (1+COEFFICIENT_RES)*velCA)/2 , velCA = temp/2; // and these
+
+			STRIKER_VELOCITYX=velSA*Math.cos(alpha)-velSP*Math.sin(alpha),STRIKER_VELOCITYZ=velSP*Math.cos(alpha)+velSA*Math.sin(alpha);
+			WHITE_VELOCITYX[i] = velCA*Math.cos(alpha) - velCP*Math.sin(alpha);
+			WHITE_VELOCITYZ[i] = velCP*Math.cos(alpha) + velCA*Math.sin(alpha);
+		}
+		for(var j=0;j<WHITE_COINS_NO;j++){
+			if(i!=j){
+				coin_coin(0,i,0,j);
+			}
+		}
+	}
+}
+function updateCoins(){
+	for(var i=0;i<BLACK_COINS_NO;i++){
+		if(BLACK_X[i]>STRIKER_MAXX || BLACK_X[i]<STRIKER_MINX){
+			BLACK_VELOCITYX_F[i]*=-1;
+		}
+		if(BLACK_Z[i]>STRIKER_MAXZ || BLACK_Z[i]<STRIKER_MINZ){
+			BLACK_VELOCITYZ_F[i]*=-1;
+		}
+		BLACK_VELOCITYX[i]*=BLACK_VELOCITYX_F[i];
+		BLACK_VELOCITYZ[i]*=BLACK_VELOCITYZ_F[i];
+		BLACK_X[i]-=BLACK_VELOCITYX[i];
+		BLACK_Z[i]+=BLACK_VELOCITYZ[i];
+		BLACK_VELOCITYX[i]=Math.max(BLACK_VELOCITYX[i]-FRICTION,0);
+		BLACK_VELOCITYZ[i]=Math.max(BLACK_VELOCITYZ[i]-FRICTION,0);
+		BLACK_SPHERE[i].position.set(BLACK_X[i],BLACK_Y[i],BLACK_Z[i]);
+		for(var j=0;j<4;j++){
+			if(Math.sqrt(Math.pow((POCKET_X[j]-BLACK_X[i]),2)+Math.pow((POCKET_Z[j]-BLACK_Z[i]),2))<(POCKET_RADIUS+COIN_RADIUS)){
+				BLACK_VISIBLE[j]=0;
+			}
+		}
+		if(BLACK_VISIBLE[i]==1)
+			scene.add(BLACK_SPHERE[i]);
+	}	
+	for(var i=0;i<WHITE_COINS_NO;i++){
+		if(WHITE_X[i]>STRIKER_MAXX || WHITE_X[i]<STRIKER_MINX){
+			WHITE_VELOCITYX_F[i]*=-1;
+		}
+		if(WHITE_Z[i]>STRIKER_MAXZ || WHITE_Z[i]<STRIKER_MINZ){
+			WHITE_VELOCITYZ_F[i]*=-1;
+		}
+		WHITE_VELOCITYX[i]*=WHITE_VELOCITYX_F[i]
+		WHITE_VELOCITYZ[i]*=WHITE_VELOCITYZ_F[i]
+		WHITE_X[i]-=WHITE_VELOCITYX[i];
+		WHITE_Z[i]+=WHITE_VELOCITYZ[i];
+		WHITE_VELOCITYX[i]=Math.max(WHITE_VELOCITYX[i]-FRICTION,0);
+		WHITE_VELOCITYZ[i]=Math.max(WHITE_VELOCITYZ[i]-FRICTION,0);
+		WHITE_SPHERE[i].position.set(WHITE_X[i],WHITE_Y[i],WHITE_Z[i]);
+		for(var j=0;j<4;j++){
+			if(Math.sqrt(Math.pow((POCKET_X[j]-WHITE_X[i]),2)+Math.pow((POCKET_Z[j]-WHITE_Z[i]),2))<(POCKET_RADIUS+COIN_RADIUS)){
+				WHITE_VISIBLE[j]=0;
+			}
+		}
+		if(WHITE_VISIBLE[i]==1)
+			scene.add(WHITE_SPHERE[i]);
+	}
+}
+function updateCamera(){
+	if(CAMERA_MOV==0){
+		camera.position.set(0,200,0);
+		camera.lookAt(new THREE.Vector3(0,0,0));
+	}
+	else if(CAMERA_MOV==1){
+		camera.position.set(200,100,0);
+		camera.lookAt(new THREE.Vector3(0,0,0));
+	}
+}
 var render = function () {
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
@@ -180,9 +347,20 @@ var render = function () {
 		CURSOR_XDIFF=(CURSOR_XDIFF+2)%CURSOR_MAXX;
 		CURSOR_X=CURSOR_MINX-CURSOR_XDIFF;
 	}
+	if(CAMERA_MOV==2){
+		camera.position.set(BLACK_X[BLACK_COINVIEWNO],100,BLACK_Z[BLACK_COINVIEWNO]);
+		camera.lookAt(new THREE.Vector3(0,0,0));
+	}
+	if(CAMERA_MOV==3){
+		camera.position.set(WHITE_X[WHITE_COINVIEWNO],100,WHITE_Z[WHITE_COINVIEWNO]);
+		camera.lookAt(new THREE.Vector3(0,0,0));
+	}
+	updateCamera();
 	updateStriker();
 	updateArrow();
 	updateCursor();
+	updateCollisions();
+	updateCoins();
 };
 
 //Movements.
@@ -208,9 +386,29 @@ function keyDownHandler(event){
 				break;
 		}
 	}
+	if(keyPressed=="1" && STRIKER_SET==1){
+		CAMERA_MOV=0;
+	}
+	if(keyPressed=="2" && STRIKER_SET==1){
+		CAMERA_MOV=1;
+	}
+	if(keyPressed=="3" && STRIKER_SET==1){
+		CAMERA_MOV=2;
+		BLACK_COINVIEWNO=(BLACK_COINVIEWNO+1)%4;
+	}
+	if(keyPressed=="4" && STRIKER_SET==1){
+		CAMERA_MOV=3;
+		WHITE_COINVIEWNO=(WHITE_COINVIEWNO+1)%4;
+	}
+	if(keyPressed=="R" && STRIKER_MOV==1){
+		striker_init();
+		add_newarrow();
+	}
 	if(event.keyCode==13 && POWER_SET==1){
 		POWER_SET=0;
-		STRIKER_VELOCITY=5*CURSOR_XDIFF/CURSOR_MAXX;
+		STRIKER_VELOCITY=7*CURSOR_XDIFF/CURSOR_MAXX;
+		STRIKER_VELOCITYX=STRIKER_VELOCITY*Math.cos(STRIKER_ANGLE);
+		STRIKER_VELOCITYZ=STRIKER_VELOCITY*Math.sin(STRIKER_ANGLE);
 		STRIKER_MOV=1;
 	}	
 	if(event.keyCode==13 && DIRECTION_SET==1){
@@ -232,6 +430,17 @@ function mouseHandler(event) {
 	MOUSE_X=event.clientX;
 	MOUSE_X=event.clientY;
 }
+var score=0;
+var text = document.createElement('div');
+text.style.position = 'absolute';
+//text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
+text.style.width = 500;
+text.style.height = 500;
+text.style.backgroundColor = "white";
+text.innerHTML = score;
+text.style.top =50 + 'px';
+text.style.left = 1650 + 'px';
+document.body.appendChild(text);
 
 //Rendering
 camera.position.set(CAMERA_X,CAMERA_Y,CAMERA_Z);
